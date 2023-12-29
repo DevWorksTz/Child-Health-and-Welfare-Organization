43d65eb37ead3b2df98ca1fd938627f026d2d3f0
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2023-12-29 21:26:10 +0300

    svg

--- chw0/src/app/Components/Home/second/page.tsx
+++ chw0/src/app/Components/Home/second/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './second.module.css'
+import Image from 'next/image'
 
 const Second = () => {
   return (
     <div className={styles.container}>
-Second
+<div className={styles.rectangle}>
+<svg width="400" height="110" className="bg-green">
+  <rect width="300" height="100" />
+</svg> 
+</div>
     </div>
   )
 }
