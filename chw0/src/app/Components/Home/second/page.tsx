ecddd952f1e0d09f809c5ed8f3f9aa6de7bebead
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-01-08 20:01:05 +0300

    Update page.tsx

--- chw0/src/app/Components/Home/second/page.tsx
+++ chw0/src/app/Components/Home/second/page.tsx
@@ -6,7 +6,7 @@ const Second = () => {
   return (
     <div className={styles.container}>
 <div className={styles.rectangle}>
-<svg width="400" height="110" className="bg-green">
+<svg width="400" height="110" className="bg-green"s>
   <rect width="300" height="100" />
 </svg> 
 </div>
