1bb5917d6a67a3b44d0128e0837e3acad369dbda
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-01-31 16:05:03 +0300

    update on third image

--- chw0/src/app/Components/Home/second/page.tsx
+++ chw0/src/app/Components/Home/second/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './second.module.css'
-import Image from 'next/image'
-
+import { League_Spartan } from 'next/font/google'
+const inter = League_Spartan({ subsets: ['latin'] })
 const Second = () => {
   return (
     <div className={styles.container}>
 <div className={styles.rectangle}>
-<svg width="400" height="110" className="bg-green"s>
-  <rect width="300" height="100" />
-</svg> 
+  <div className={styles.text1}>
+    
+    <svg width="550" height="110" className="bg-neutral-950 ml-11 opacity-80 border-rounded ">
+</svg><h1 className={styles.text}>KEY FOCUS AREAS</h1></div>
+
 </div>
+
     </div>
   )
 }
