e9905c270007d6ff832688a84a3f4eeed0ce9069
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-02-02 19:29:31 +0300

    navbar hamburger menu hotfix

--- chw0/src/app/Components/Home/third/page.tsx
+++ chw0/src/app/Components/Home/third/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import styles from './third.module.css'
+import { League_Spartan } from 'next/font/google'
+const inter = League_Spartan({ subsets: ['latin'] })
 
 const Third = () => {
   return (
-    <div className={styles.container}>
-        Third
+    <div className={styles.third}>
+  
+        <div className={styles.container}>
+        <div className={styles.header}>
+        <h1 className={inter.className}>News</h1>
+      </div>
+      <div className={styles.grids}>
+          <div className={styles.cols}>
+            hello brother
+          </div>
+          <div className={styles.cols}>
+            hello brother
+          </div>
+          <div className={styles.cols}>
+            hello brother
+          </div>
+        </div>
+        </div>
         </div>
   )
 }
