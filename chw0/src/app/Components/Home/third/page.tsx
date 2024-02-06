3dfe7035480bd084fe2bc01871620d00f98bf9ee
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-02-06 17:07:06 +0300

    news tab update

--- chw0/src/app/Components/Home/third/page.tsx
+++ chw0/src/app/Components/Home/third/page.tsx
@@ -2,42 +2,94 @@ import React from 'react'
 import styles from './third.module.css'
 import { League_Spartan } from 'next/font/google'
 const inter = League_Spartan({ subsets: ['latin'] })
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from 'next/image';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+
 
 const Third = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0.3, 0], [1, 0.5]);
   return (
     <div className={styles.third}>
-  
+   <motion.div  style={{
+          scale
+        }} >
         <div className={styles.container}>
         <div className={styles.header}>
         <h1 className={inter.className}>News</h1>
       </div>
       <div className={styles.grids}>
           <div className={styles.cols}>
-            <div className={styles.fig}>
-              IMAGE
-            </div>
-            <div className={styles.link}>
-              TITLE TO LINK
-            </div>
+            <Card className="bg-orange-200">
+            <CardHeader>
+                <CardTitle>NEWS TITLE ONE</CardTitle>
+                <CardDescription>Date</CardDescription>
+            </CardHeader>
+            <CardContent >
+            <Image
+                  src="/messina.jpg"
+                  width={400}
+                  height={400}
+                  alt="Picture of the author"
+                  border-radius="10px"
+                />
+            </CardContent>
+            <CardFooter>
+                  <p>read more</p>
+            </CardFooter>
+            </Card>
           </div>
           <div className={styles.cols}>
-          <div className={styles.fig}>
-              IMAGE
-            </div>
-            <div className={styles.link}>
-              TITLE TO LINK
-            </div>
+          <Card className="bg-orange-200">
+          <CardHeader>
+              <CardTitle>NEWS TITLE TWO</CardTitle>
+              <CardDescription>Date</CardDescription>
+          </CardHeader>
+          <CardContent>
+          <Image
+                  src="/consoler.jpg"
+                  width={400}
+                  height={400}
+                  alt="Picture of the author"
+                  border-radius="10px"
+                />
+          </CardContent>
+          <CardFooter>
+          <p>read more</p>
+          </CardFooter>
+          </Card>
           </div>
           <div className={styles.cols}>
-          <div className={styles.fig}>
-              IMAGE
-            </div>
-            <div className={styles.link}>
-              TITLE TO LINK
-            </div>
+          <Card className="bg-orange-200">
+          <CardHeader>
+              <CardTitle>NEWS TITLE THREE</CardTitle>
+              <CardDescription>Date</CardDescription>
+          </CardHeader>
+          <CardContent>
+          <Image
+                  src="/hey.jpg"
+                  width={400}
+                  height={400}
+                  alt="Picture of the author"
+                  border-radius="10px"
+                />
+          </CardContent>
+          <CardFooter>
+          <p>read more</p>
+          </CardFooter>
+          </Card>
           </div>
         </div>
         </div>
+        </motion.div>
         </div>
   )
 }
