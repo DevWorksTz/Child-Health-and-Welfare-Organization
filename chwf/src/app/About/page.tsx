6e692d51d791f7af4cebb196a81c4909a1567fff
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-03-23 20:45:05 +0300

    ff

--- chwf/src/app/About/page.tsx
+++ chwf/src/app/About/page.tsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import styles from './about.module.css';
-import Navbar from '../Components/Nav/page';
-import Contact from '../Contact/page';
-const page = () => {
-  return (
-  <div className={styles.container}>
- <div className={styles.about}>
- is Lorem Ipsum?
- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
- 
- Why do we use it?
- It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
- 
- 
- Where does it come from?
- Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
- 
- The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
- 
- Where can I get some?
- There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
- Where does it come from?
- Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
- 
- The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
- 
- Where can I get some?
- There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
- Where does it come from?
- Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.
- </div>
-</div>
+import React from 'react';
+import styles from './AboutUs.module.css';
 
+const AboutUs = () => {
+  return (
+    <div className={styles.about}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Know about us</h1>
+      <div className={styles.content}>
+        <p>
+          Child Health and Welfare is a non-profit organization dedicated to promoting
+          the health and well-being of children. We work tirelessly to ensure
+          every child has access to the resources they need to thrive.
+        </p>
+        <p>
+          Our mission is promoting holistic child development and fostering a brighter future by providing comprehensive support, empowering communities, and advocating for policies that prioritize child health and welfare outcomes. We achieve this by:
+        </p>
+        <ul className={styles.list}>
+          <li>Providing [List of services provided]</li>
+          <li>Supporting [Beneficiaries of your services]</li>
+          <li>Advocating for [Causes you advocate for]</li>
+        </ul>
+        <p>
+          With the help of our dedicated staff, volunteers, and generous donors,
+          we have made a significant impact on the lives of countless children.
+        </p>
+      </div>
+      <button className={styles.button}>Get Involved</button>
+    </div>
+    </div>
   );
 };
 
-export default page
+export default AboutUs;
--- chwf/src/app/Events/page.tsx
+++ chwf/src/app/Events/page.tsx
@@ -4,18 +4,18 @@ import Timeline from './Timeline';
 import styles from'./SpeechBubbles.module.css'
 
 const events = [
-  { title: 'Event 1', description: 'Description for event 1', date: '2023-01-01' ,side:'left'},
-  { title: 'Event 2', description: 'Description for event 2', date: '2023-06-15' ,side:'right'},
-  { title: 'Event 3', description: 'Description for event 3', date: '2024-02-20' ,side:'left'},
-  { title: 'Event 1', description: 'Description for event 1', date: '2023-01-01' ,side:'right'},
-  { title: 'Event 2', description: 'Description for event 2', date: '2023-06-15' ,side:'left'},
-  { title: 'Event 3', description: 'Description for event 3', date: '2024-02-20' ,side:'right'},
-  { title: 'Event 1', description: 'Description for event 1', date: '2023-01-01' ,side:'left'},
-  { title: 'Event 2', description: 'Description for event 2', date: '2023-06-15' ,side:'right'},
-  { title: 'Event 3', description: 'Description for event 3', date: '2024-02-20' ,side:'left'},
-  { title: 'Event 1', description: 'Description for event 1', date: '2023-01-01' ,side:'right'},
-  { title: 'Event 2', description: 'Description for event 2', date: '2023-06-15' ,side:'left'},
-  { title: 'Event 3', description: 'Description for event 3', date: '2024-02-20' ,side:'right'},
+  { title: 'Zero Discrimination Day', description: 'Description for event 1', date: '2024-03-01' ,side:'left'},
+  { title: 'World Birth Defect Day', description: 'Description for event 2', date: '2024-03-03' ,side:'right'},
+  { title: 'World Hearing Day', description: 'Description for event 3', date: '2024-03-03' ,side:'right'},
+  { title: 'World Obesity Day', description: 'Description for event 1', date: '2023-03-04' ,side:'left'},
+  { title: 'World Glaucoma Day', description: 'Description for event 2', date: '2023-03-15 to 2023-03-17' ,side:'right'},
+  { title: 'International Womens Day', description: 'Description for event 3', date: '2024-03-08' ,side:'left'},
+  { title: 'World Kidney Day', description: 'Description for event 1', date: '2023-03-14' ,side:'right'},
+  { title: 'International Day for the Elimination ', description: ' of Racial Discrimination', date: '2023-03-21' ,side:'left'},
+  { title: 'World Down Syndrome Day', description: 'Description for event 3', date: '2024-03-21' ,side:'right'},
+  { title: 'World Water Day', description: 'Description for event 1', date: '2023-03-22' ,side:'left'},
+  { title: 'World Tuberculosis', description: 'Description for event 2', date: '2023-03-24' ,side:'right'},
+  { title: 'International Day of Solidarity with Detained ', description: ' and Missing Staff Members', date: '2024-03-25' ,side:'left'},
 ];
 
 const IndexPage = () => {
