6270841ea9447ad01e36f0aebabfc366582efea1
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-03-24 04:01:30 +0300

    Update Timeline.tsx

--- chwf/src/app/Events/Timeline.tsx
+++ chwf/src/app/Events/Timeline.tsx
@@ -1,7 +1,7 @@
 // components/Timeline.js
 import React from 'react';
 import styles from'./VerticalLine.module.css'
-const Timeline = ({ events }:) => {
+const Timeline = ({ events }:{[key:string]:any}) => {
   return (
     <div className={styles.timeline}>
       <ul>
