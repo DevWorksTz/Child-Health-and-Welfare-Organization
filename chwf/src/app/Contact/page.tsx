214ba09fdf465f05027c65bfc3a213fd987e9de0
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-03-24 03:50:06 +0300

    Update page.tsx

--- chwf/src/app/Contact/page.tsx
+++ chwf/src/app/Contact/page.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
  <div className={styles.container}>
  <div className={styles.text}>
-<p>Hello there! We're thrilled to hear from you and are eager to connect. Whether you have questions, suggestions, or simply want to say hello, we're here to listen.</p>
+<p>Hello there! We are thrilled to hear from you and are eager to connect. Whether you have questions, suggestions, or simply want to say hello, we're here to listen.</p>
 <p>
 How to Reach Us:
 </p>
@@ -15,19 +15,19 @@ Email: Feel free to drop us a line at email@example.com. We check our inbox regu
 Phone: Have something urgent to discuss? Give us a call at +123-456-7890 during our office hours. Our friendly team is ready to assist you.
 </p>
 <p>
-Visit Us: If you're in the neighborhood, we'd love for you to stop by and say hello! Our address is [123 Sunshine Avenue, Happytown, ABC 12345]. We're always excited to meet new friends.
+Visit Us: If you are in the neighborhood, we would love for you to stop by and say hello! Our address is [123 Sunshine Avenue, Happytown, ABC 12345]. We're always excited to meet new friends.
 </p>
 <p>
 Social Media: Connect with us on social media platforms like Facebook, Twitter, and Instagram [@ChildrensFoundation]. Stay updated on our latest activities, events, and initiatives!
 </p>
 <p>
-Feedback: Your feedback is invaluable to us. Let us know how we're doing or share your ideas for improving our programs. We're all ears!
+Feedback: Your feedback is invaluable to us. Let us know how we are doing or share your ideas for improving our programs. We are all ears!
 </p>
 <p>
-Get Involved: Interested in volunteering, donating, or collaborating with us? We'd love to hear from you! Reach out, and let's make a positive difference together.
+Get Involved: Interested in volunteering, donating, or collaborating with us? We wouldd love to hear from you! Reach out, and let us make a positive difference together.
 </p>
 <p>
-Thank you for considering reaching out to us. Your support means the world to the children we serve. Let's continue spreading love, kindness, and hope!
+Thank you for considering reaching out to us. Your support means the world to the children we serve. Let us continue spreading love, kindness, and hope!
 </p>
 <p>
 Warm regards,
